c00044e53240a7017d57be8e134792ad36637814
swandevson <43706436+swandevson@users.noreply.github.com>
2024-09-23 01:56:13 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 108 ms, Memory: 14560 KB -BaekjoonHub

--- Java/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ Java/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -20,49 +20,14 @@ public class Main {
 				dp[length++] = arr[i];
 				continue;
 			}
-			int idx = Arrays.binarySearch(dp, 0, length - 1, arr[i]);
-			if (idx < 0) {
-				idx = -idx - 1;
+			if (arr[i] == dp[length-1]) {
+				continue;
 			}
+			int idx = Arrays.binarySearch(dp, 0, length-1, arr[i]);
+			idx = idx < 0 ? -idx-1 : idx;
 			dp[idx] = arr[i];
 		}
 
 		System.out.println(length);
-
-
-		// lis_dp(dp, arr, n);
-	}
-
-	static int binary_search(int[] lis, int left, int right, int target) {
-		int mid;
-
-		while (left < right) {
-			mid = (left + right) / 2;
-
-			if(lis[mid] < target) {
-				left = mid + 1;
-			}
-			else {
-				right = mid;
-			}
-		}
-
-		return right;
-	}
-
-	static int lis_dp(int[] lis, int[] arr, int N) {
-		lis[0] = arr[0];
-
-		int len = 1;
-		for (int i = 1; i < N; i++) {
-			if (lis[len - 1] < arr[i]) {
-				lis[len++] = arr[i];
-			}
-			else if (lis[len - 1] > arr[i]){
-				int idx = binary_search(lis, 0, len - 1, arr[i]);
-				lis[idx] = arr[i];
-			}
-		}
-		return len;
 	}
 }
